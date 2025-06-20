f9461e848129d6e79d77aac121d70540252f526b
Dikshantashresth <shresthadikshanta25@gmail.com>
2025-06-20 17:23:10 +0545

    v1

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,28 @@
-import {
-  createBrowserRouter,
-  
-  RouterProvider,
-} from 'react-router-dom';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import { Home, AnimeDetail, Searchfeed } from './Components';
 import Overview from './Components/Overview';
 import Producers from './Components/Producers';
 import Character from './Components/Character';
 import Trendingfeed from './Components/Trendingfeed';
 import List from './Components/List';
-import './App.css'
 import { Watchlistprovider } from './Components/context/context.jsx';
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Home />, // or any default component you want
-  },
-  {
-    path: '/trending',
-    element: <Trendingfeed />
-  },
-  {
-    path: '/anime/:id',
-    element: <AnimeDetail />,
-    children: [
-      {
-        index: true,
-        element: <Overview />,
-      },
-      {
-        path: 'characters',
-        element: <Character />,
-      },
-      {
-        path: 'producers',
-        element: <Producers />,
-      },
-    ],
-  },
-  {
-    path: '/search/:name',
-    element: <Searchfeed />,
-  },
-  {
-    path: '/mylist',
-    element: <List />
-  }
-]);
-
 
 export default function App() {
   return (
     <Watchlistprovider>
-      <RouterProvider router={router} />
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/trending" element={<Trendingfeed />} />
+          <Route path="/search/:name" element={<Searchfeed />} />
+          <Route path="/mylist" element={<List />} />
+          <Route path="/anime/:id" element={<AnimeDetail />}>
+            <Route index element={<Overview />} />
+            <Route path="characters" element={<Character />} />
+            <Route path="producers" element={<Producers />} />
+          </Route>
+        </Routes>
+      </HashRouter>
     </Watchlistprovider>
   );
 }
